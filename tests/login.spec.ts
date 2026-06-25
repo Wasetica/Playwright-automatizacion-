dd2079412b837b27324ed3ead18bc0dc7be2e2be
Wasetica <sebas121985xd@gmail.com>
2026-06-25 18:39:39 -0500

    Login correcto e incorrecto

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,16 +1,29 @@
-import {expect, test } from 'playwright/test'
+import { test, expect } from '@playwright/test'
 
+test.describe('Pruebas de login OrangeHRM', () => {
 
-test('login to hrm', async ({ page }) => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://opensource-demo.orangehrmlive.com/')
+    })
 
+    test('login correcto', async ({ page }) => {
 
-    await page.goto('https://opensource-demo.orangehrmlive.com/')
-    await page.getByRole('textbox',{name: 'Username'}).fill('Admin')
-    await page.getByRole('textbox',{name: 'Password'}).fill('admin123')
-    await page.getByRole('button',{name: 'Login'}).click()
+        await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+        await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+        await page.getByRole('button', { name: 'Login' }).click()
 
-    await expect(page.getByRole('link',{name: 'Admin'})).toBeVisible()
+        await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
 
+    })
 
+    test('login incorrecto', async ({ page }) => {
+
+        await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+        await page.getByRole('textbox', { name: 'Password' }).fill('claveIncorrecta')
+        await page.getByRole('button', { name: 'Login' }).click()
+
+        await expect(page.getByText('Invalid credentials')).toBeVisible()
+
+    })
 
 })
